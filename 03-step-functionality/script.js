f37f1d6230dc76e84bb90a59013c022c1bac0bb2
elementtest <elementtest7@gmail.com>
2023-03-03 11:23:59 +0800

    add js update

--- 03-step-functionality/script.js
+++ 03-step-functionality/script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circles = document.querySelectorAll('circle')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
@@ -10,5 +10,17 @@ next.addEventListener('click', () => {
 
   if (currentActive > circles.length) {
     currentActive = circles.length
+    //resetting the currentActive so it does NOT go to more than four.  beause there are ONLY FOUR circles
   }
+  update()
+})
+
+prev.addEventListener('click', () => {
+  currentActive--
+
+  if (currentActive < 1) {
+    currentActive = 1
+    //makes it so you cant go back before the number one as far as the circles go
+  }
+  update()
 })
