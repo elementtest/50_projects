b286f707330e5c6a69fd2e7bffbb557f780927a0
elementtest <elementtest7@gmail.com>
2023-04-01 00:54:22 +0800

    js

--- 09-wave-login/script.js
+++ 09-wave-login/script.js
@@ -1,7 +1,13 @@
 const labels = document.querySelectorAll('.form-control label')
-
+//this retrieves two strings from the html
+// Email
+// and Password strings
+// and then i think it puts them in a node list.. it isnt an array yet yes that is correct confirmed via console log
 labels.forEach((label) => {
+  console.log(label)
   label.innerHTML = label.innerText
+    //innerHTML will interpret the tags as html... innertext will not do this... so innerHTML will be open to hacking attacks because HTML can be injected into the page.  innertext simplifies it.  without it... the following code will fail because we just want to put a map function on each of the letters
+    //this is important, without it the code does not work
     .split('')
     .map(
       (letter, idx) =>
