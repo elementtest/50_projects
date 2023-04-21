d907712d45b0f32a6f8bb4756c84408f6f86f7a7
elementtest <elementtest7@gmail.com>
2023-04-21 15:26:45 +0800

    js

--- 11-jokes/script.js
+++ 11-jokes/script.js
@@ -7,7 +7,11 @@ function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
+      /* accept is the key and application/json is the value
+	  all of this is an object the headers: {INSERT STUFF} is just one big object */
     },
   }
   fetch('https://icanhazdadjoke.com/', config)
+    .then((res) => res.json())
+    .then((date) => console.log(date))
 }
