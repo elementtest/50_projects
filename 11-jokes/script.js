a8787c66a9ed24745a1572d860c5e2df50b52e02
elementtest <elementtest7@gmail.com>
2023-04-27 07:38:53 +0800

    final js

--- 11-jokes/script.js
+++ 11-jokes/script.js
@@ -1,19 +1,39 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
+jokeBtn.addEventListener('click', generateJoke)
+
 generateJoke()
 
-function generateJoke() {
+//USING ASYNC/AWAIT
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
-      /* accept is the key and application/json is the value
-	  all of this is an object the headers: {INSERT STUFF} is just one big object */
     },
   }
-  fetch('https://icanhazdadjoke.com/', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke
-    })
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
+
+/* 
+USING .then()
+function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	}
+
+	fetch('https://icanhazdadjoke.com', config)
+	.then((rez) => res.json())
+	.then((data) => {
+		jokeEl.innerHTML = data.joke
+	})
+}
+
+*/
