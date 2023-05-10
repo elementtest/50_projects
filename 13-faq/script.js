37617f10c2a06da6e66bc4feb02aeec496bda698
elementtest <elementtest7@gmail.com>
2023-05-10 11:20:22 +0800

    css

--- 13-faq/script.js
+++ 13-faq/script.js
@@ -1,5 +1,5 @@
 const toggles = document.querySelectorAll('.faq-toggle')
-
+//selects the down arrow and x next to each joke to reveal the punch line
 toggles.forEach((toggle) => {
   toggle.addEventListener('click', () => {
     toggle.parentNode.classList.toggle('active')
