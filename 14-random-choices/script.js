72271eb648c2dceb0713c443de29ff4b5aea4925
elementtest <elementtest7@gmail.com>
2023-05-17 11:20:14 +0800

    comment

--- 14-random-choices/script.js
+++ 14-random-choices/script.js
@@ -6,4 +6,8 @@ const textarea = document.getElementById('textarea')
 // targets the white input box to input text in order to generate the choices
 textarea.focus()
 //why does this need focus?
+//^^^^ answer::: the javascript loads on the page THEN
+// the textarea input white box is IMMEDIATELY FOCUSED
+// try it out... if it is unfocused the text area will be yellow
+// then focused the color will change to pink
 //the focused element is the element that will receive keyboard and similar events by default
