6f2e68fccb0c0847b7248f0ec17ca42af8e506e9
elementtest <elementtest7@gmail.com>
2023-05-17 11:32:19 +0800

    js

--- 14-random-choices/script.js
+++ 14-random-choices/script.js
@@ -11,3 +11,43 @@ textarea.focus()
 // try it out... if it is unfocused the text area will be yellow
 // then focused the color will change to pink
 //the focused element is the element that will receive keyboard and similar events by default
+
+textarea.addEventListener('keyup', (e) => {
+	createTags(e.target.value) 
+
+	if(e.key === 'Enter') {
+		setTimeout(() => {
+		e.target.value = ''	
+		}, 10)
+		randomSelect()
+	}
+})
+
+function createTags(input) {
+	const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+	tagsEl.innerHTML = ''
+
+	tags.forEach(tag => {
+		const tagEl = document.createElement('span')
+		tagEl.classList.add('tag')
+		tagEl.innerText = tag
+		tagsEl.appendChild(tagEl)
+	})
+}
+
+function randomSelect() {
+	const times = 30 
+
+	const interval = setInterval(() = {
+		const randomTag = pickRandomTag()
+
+		if (randomTag !== undefined) {
+			highlightTag(randomTag) 
+
+			setTimeout(() => {
+				unHighlightTag(randomTag)
+			}, 100)
+		}
+	}, 100)
+}
