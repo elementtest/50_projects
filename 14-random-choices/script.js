f8f6dc6b60705380aa2cf21cb3b9856a68dd7914
elementtest <elementtest7@gmail.com>
2023-05-24 14:38:30 +0800

    complete

--- 14-random-choices/script.js
+++ 14-random-choices/script.js
@@ -61,6 +61,16 @@ function randomSelect() {
       UnhighlightTag(randomTag)
     }, 100)
   }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
 }
 
 function pickRandomTag() {
