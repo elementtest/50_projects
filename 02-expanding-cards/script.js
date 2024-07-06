df82fbb757cc42c223abd6cb955d201d4015453d
elementtest <elementtest8@gmail.com>
2024-07-06 13:33:50 +0800

    9077177426509

--- 02-expanding-cards/script.js
+++ 02-expanding-cards/script.js
@@ -9,16 +9,9 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
-    removeActiveClasses();
+    panel.classList.toggle('active')
     //when you click will invoke function of removing the active class
-    panel.classList.add("active");
   });
 });
 
-function removeActiveClasses() {
-  //create function that removes the active class from whatever element
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
-}
 
