651b39a38ad9c216268b29c8f8e735077c0f27f5
elementtest <elementtest8@gmail.com>
2024-08-19 08:52:06 +0800

    13883666225558

--- 09-wave-login/script.js
+++ 09-wave-login/script.js
@@ -1,18 +1,8 @@
-const labels = document.querySelectorAll('.form-control label')
-//this retrieves two strings from the html
-// Email
-// and Password strings
-// and then i think it puts them in a node list.. it isnt an array yet yes that is correct confirmed via console log
-labels.forEach((label) => {
-  console.log(label)
-  label.innerHTML = label.innerText
-    //innerHTML will interpret the tags as html... innertext will not do this... so innerHTML will be open to hacking attacks because HTML can be injected into the page.  innertext simplifies it.  without it... the following code will fail because we just want to put a map function on each of the letters
-    //this is important, without it the code does not work
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(onelabel => {
+    onelabel.innerHTML = onelabel.innerText
     .split('')
-    .map(
-      (letter, idx) =>
-        `<span style ="transition-delay:${idx * 50}ms">${letter}</span>`
-    )
-    /* think of map as like taking a circle and converting it into a square and this is run on each letter from the foreach method */
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
     .join('')
 })
